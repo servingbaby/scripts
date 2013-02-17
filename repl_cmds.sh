4d8f8411186bf6e552daac56da0d34c77ac75f02
troyengel <troyengel@gmail.com>
2013-02-17 14:39:38 -0600

    add perl and python, formatting

--- repl_cmds.sh
+++ repl_cmds.sh
@@ -24,10 +24,19 @@
 
 alias tsize='shopt -s checkwinsize;COLUMNS=$(tput cols);LINES=$(tput lines);export COLUMNS LINES;echo -e "COLUMNS=$COLUMNS;\nLINES=$LINES;\nexport COLUMNS LINES;"'
 
-# if netcat is not available (requires --enable-net-redirections compiled in
-# with bash, works on RHEL/CentOS/Fedora but not Ubuntu/Debian
+# if netcat is not available
+# - requires --enable-net-redirections compiled in with bash
+# - works on RHEL/CentOS/Fedora but not Ubuntu/Debian
 
+# tcp
 function nctzv() { [ $# -eq 2 ] && (timeout 3 bash -c "echo >/dev/tcp/$1/$2" && echo "Connection to $1 port $2/tcp succeeded" || echo "Connection to $1 port $2/tcp failed"); }
 
+# udp
 function ncuzv() { [ $# -eq 2 ] && (timeout 3 bash -c "echo >/dev/udp/$1/$2" && echo "Connection to $1 port $2/udp succeeded" || echo "Connection to $1 port $2/udp failed"); }
+
+# the same nctzv() function in perl
+function nctzvi_pl() { perl -e 'use IO::Socket::INET;$socket=IO::Socket::INET->new(Proto=>tcp,Timeout=>3,PeerAddr=>$ARGV[0],PeerPort=>$ARGV[1]);printf("Connection to %s port %s/tcp ",$ARGV[0],$ARGV[1]);if(defined $socket && $socket){$socket->close();print "succeeded\n"}else{print "failed\n";}' $1 $2; }
+
+# the same nctzv() function in python
+function nctzv_py() { python -c "exec('import sys\nimport socket\nh=sys.argv[1]\np=sys.argv[2]\ns=socket.socket(socket.AF_INET,socket.SOCK_STREAM)\ns.settimeout(3)\ntry:\n\ts.connect((sys.argv[1],int(sys.argv[2])))\n\ts.shutdown(2)\n\tprint \"Connection to \"+h+\" port \"+p+\"/tcp succeeded\"\nexcept:\n\tprint \"Connection to \"+h+\" port \"+p+\"/tcp failed\"\ns.close')" $1 $2; }
 
